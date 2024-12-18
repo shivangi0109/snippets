f3e497af4ca136b28feed5c8c683d2d10e74b9b1
Shivangi Srivastava <shi06dec@gmail.com>
2024-12-18 16:41:28 -0700

    Customize Not Found Page

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function SnippetNotFound() {
+  return (
+    <div className="text-3xl bold flex items-center justify-center">
+      <h1>Sorry, but we couldnt find that particular snippet</h1>
+    </div>
+  )
+}
