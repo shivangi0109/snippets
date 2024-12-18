e34499c4b2784a165f32b94699b71145792136c1
Shivangi Srivastava <shi06dec@gmail.com>
2024-12-18 16:32:57 -0700

    Fetch particular record to show the snippet

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,7 +1,24 @@
-export default function SnippetShowPage(props: any) {
-  console.log(props);
-  
+import { notFound } from 'next/navigation';
+import { db } from '@/db';
+
+interface SnippetShowPageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  const { id } = await props.params;
+ 
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(id) },
+  });
+
+  if(!snippet) {
+    return notFound();
+  }
+
   return (
-    <div>Show a snippet</div>
+    <div>{snippet.title}</div>
   );
 }
