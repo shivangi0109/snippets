e0f7764cd613bf88e93c51c5d6f0bafb15cb67ac
Shivangi Srivastava <shi06dec@gmail.com>
2024-12-19 17:50:03 -0700

    Fetch the id in the snippet edit page

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,13 +1,25 @@
+import { notFound } from 'next/navigation';
+import  { db } from '@/db';
+
 interface SnippetEditPageProps {
-  params: {
-    id: string
-  }
+  params: Promise<{
+    id: string;
+  }>;
 }
 
-export default function SnippetEditPage(props: SnippetEditPageProps) {
-  const id = parseInt(props.params.id);
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
+  const { id } = await props.params;
+ 
+  const snippetId = parseInt(id);
+  const snippet = await db.snippet.findFirst({
+    where: { id: snippetId },
+  });
+
+  if(!snippet) {
+    return notFound();
+  }
 
   return (
-    <div>Editing snippet with id {id}</div>
+    <div>Editing snippet with title {snippet.title}</div>
   )
 }
